d027b15addb0374ae9455014371c7162d6d88c7f
Whatley <Rwhatley@bju.edu>
2016-02-18 14:55:21 -0500

    autowired v2 injection demo

--- src/AppConfig.java
+++ src/AppConfig.java
@@ -20,10 +20,10 @@ public class AppConfig {
 		 return customerService;
 	 }
 	
-	@Bean(name = "customerRepository")
-	public CustomerRepository getCustomerRepository(){
-		return new HibernateCustomerRepositoryImpl();
-	}
+//	@Bean(name = "customerRepository")
+//	public CustomerRepository getCustomerRepository(){
+//		return new HibernateCustomerRepositoryImpl();
+//	}
 	
 	
 }
